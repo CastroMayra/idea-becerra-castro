706325741cd4c5cd3ee29eaa57101e6f26d79fa3
Mayra Castro <castrobmayra@gmail.com>
2022-01-10 17:43:31 -0300

    Se agrega Menú

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function NavBar() {
+        return (
+            <>
+                <div>
+                    <ul>
+                        <li><a href="#">INICIO</a></li>
+                        <li><a href="#">MASCOTAS</a></li>
+                        <li><a href="#">CONTACTO</a></li>
+                    </ul>
+                </div>
+            </>
+
+        )
+}
