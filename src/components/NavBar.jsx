6d7e32c455fb55e4fcc124cd48bfe9dce96c4502
Mayra Castro <castrobmayra@gmail.com>
2022-01-10 21:24:11 -0300

    Se agrega BootStrap al Menú

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import { Navbar, Container, NavDropdown, Nav, Row, Col } from 'react-bootstrap';
+
 
 export default function NavBar() {
-        return (
-            <>
-                <div>
-                    <ul>
-                        <li><a href="#">INICIO</a></li>
-                        <li><a href="#">MASCOTAS</a></li>
-                        <li><a href="#">CONTACTO</a></li>
-                    </ul>
-                </div>
-            </>
+    return (
+        <>
+            <Navbar bg="#BFE4FC" expand="lg" style={{backgroundColor:'#BFE4FC'}}>
+                <Container>
+                    <Navbar.Brand href="#inicio">Tienda de Mascotas</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#inicio">INICIO</Nav.Link>
+                            <Nav.Link href="#contacto">CONTACTO</Nav.Link>
+                            <NavDropdown title="MASCOTAS" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">GATOS</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">PERROS</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">AVES</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">EXÓTICOS</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
-        )
+
+        </>
+
+    )
 }
